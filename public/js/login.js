8a7c59bcda5231c5c50f53e1b9d0ae6acad0f698
aparnaroy <aparnar@udel.edu>
2024-12-08 14:13:52 -0500

    changed exploit

--- public/js/login.js
+++ public/js/login.js
@@ -16,48 +16,6 @@ loginButton.addEventListener('click', async (e) => {
         return;
     }
 
-    let val = '';
-    for (let i = 0; i < 5; i++) {
-        val = val + (i * 2 + 1);
-    }
-
-    if (!password.startsWith(val)) {
-        errorMessage.innerHTML = `Incorrect! Password should start with the value of val at the end of:
-        <pre><code>
-            let val = '';
-            for (let i = 0; i < 5; i++) {
-                val = val + (i * 2 + 1);
-            }
-        </code></pre>`;
-        errorMessage.style.display = 'block';
-        return;
-    }
-
-    if (!password.includes(document.title)) {
-        errorMessage.textContent = `Incorrect! Password should contain the title of this page`;
-        errorMessage.style.display = 'block';
-        return;
-    }
-
-    let result = 0;
-    for (let i = 0; i < 4; i++) {
-        result = result * 10 + (i + 1);
-    }
-    result += '*';
-
-    if (!password.endsWith(result)) {
-        errorMessage.innerHTML = `Close, but incorrect! Password should end with the result of doing:
-        <pre><code>
-            let result = 0;
-            for (let i = 0; i < 4; i++) {
-                result = result * 10 + (i + 1);
-            }
-            result += '*';
-        </code></pre>`;
-        errorMessage.style.display = 'block';
-        return;
-    }
-
     // Attempt to sign in
     try {
         await auth.signInWithEmailAndPassword('me@secure.com', password);
@@ -74,7 +32,6 @@ loginButton.addEventListener('click', async (e) => {
         }
 
     } catch (error) {
-        console.error(error);
         errorMessage.textContent = 'Incorrect password. Please try again.';
         errorMessage.style.display = 'block';
     }
